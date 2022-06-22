38edc312a81f40b0baa1742f7d7fa3dddeaf8c62
SaBobic <fabrizioettori@gmail.com>
2022-06-22 16:52:36 +0200

    added reset button script

--- js/script.js
+++ js/script.js
@@ -20,18 +20,28 @@ Il recap dei dati e l'output del prezzo finale va stampato in pagina formattato 
 let pricePerKm = 0.21;
 let calculateButton = document.getElementById('calculate');
 let resetButton = document.getElementById('reset');
+let nameInput;
+let kmsInput;
+let ageInput;
 
-// 2.
+// 2. Registrare al click gli input nome e cognome, km da percorrere e fascia d'età
 calculateButton.addEventListener('click', function(){
-    let nameInput = document.getElementById('name');
+    nameInput = document.getElementById('name');
     name = nameInput.value;
     console.log("L'utente si chiama " + name);
     
-    let kmsInput = document.getElementById('kms');
+    kmsInput = document.getElementById('kms');
     kms = parseInt(kmsInput.value);
-    console.log("L'utente deve percorrere" + kms + " chilometri");
+    console.log("L'utente deve percorrere " + kms + " chilometri");
 
-    let ageInput = document.getElementById('age');
+    ageInput = document.getElementById('age');
     age = ageInput.value;
     console.log("L'utente è " + age);
+})
+
+// 3. Settare il pulsante "reset"
+resetButton.addEventListener('click', function(){
+    nameInput.value = "";
+    kmsInput.value = "";
+    ageInput.value = "";
 })
