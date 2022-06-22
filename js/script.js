1c15b98fae8325d2c0d3160d79609feea637b998
SaBobic <fabrizioettori@gmail.com>
2022-06-22 22:59:35 +0200

    added validation

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ Il recap dei dati e l'output del prezzo finale va stampato in pagina formattato 
 2. Registrare al click gli input nome e cognome, km da percorrere e fascia d'età
 3. Stampare su schermo il riepilogo dati
 4. Settare il pulsante "reset"
-5. Aggiungere la validazione
+5. Validazione
 */
 
 // 1. Dichiarare le variabili globali
@@ -28,51 +28,76 @@ const finalPrice = document.getElementById('final-price');
 const cab = document.getElementById('cab');
 const cpCode = document.getElementById('cp-code');
 
-// 2. Registrare al click gli input nome e cognome, km da percorrere e fascia d'età
-// 3. Stampare su schermo il riepilogo dati
+// 2. Registrare al click gli input
 calculateButton.addEventListener('click', function(){
-    // Selezione più stampa nome passeggero
+
+    // Registrazione value nome passeggero
     let passenger = document.getElementById('passenger');
     let name = nameInput.value;
     console.log("nome: " + name);
-    passenger.innerText = name;
-
-    // Selezione più stampa chilometri
+    
+    // Registrazione value chilometri
     kms = parseInt(kmsInput.value);
     console.log("chilometri: " + kms);
-
-    // Selezione più stampa tariffa in base all'età
+    
+    // Registrazione value tariffa in base all'età
     let offer = document.getElementById('offer');
     let age = ageInput.options[ageInput.selectedIndex].value;
     console.log("età: " + age);
-    let offerMessage = "Tariffa Standard";
 
-    if (age === "minorenne") offerMessage = "Tariffa Minorenni";
-    if (age === "over-65") offerMessage = "Tariffa Over 65";
     
-    offer.innerText = offerMessage;
-
-    // Selezione più stampa costo biglietto in base alla tariffa
-    let price = pricePerKm * kms;
-    console.log("Prezzo pieno: " + price);
-
-    if (age === "minorenne") price *= offerMinor;
-    if (age === "over-65") price *= offerOver65;
+    // 5. Validazione
+    let isValid = true;
+    let errorMessage;
     
-    finalPrice.innerText = price.toFixed(2) + "€";
+    if (name === "" || !isNaN(name)){
+        isValid = false;
+        errorMessage = "Nome e Cognome non inseriti correttamente.";
+    }
 
-    // Selezione più stampa numero cabina
-    const cabMin = 1;
-    const cabMax = 9;
-    cab.innerText = Math.floor(Math.random() * (cabMax + 1 - cabMin)) + cabMin;
+    if (kms < 1 || isNaN(kms)){
+        isValid = false;
+        errorMessage = "Chilometri non inseriti correttamente.";
+    }
 
-    // Selezione più stampa codice CP
-    const cpMin = 10000;
-    const cpMax = 20000;
-    cpCode.innerText = Math.floor(Math.random() * (cpMax + 1 - cpMin)) + cpMin;
+    if (age === ""){
+        isValid = false;
+        errorMessage = "Fascia d'età non inserita correttamente.";
+    }
+
+    if (isValid){
+
+        // Registrazione messaggio in base alla tariffa
+        let offerMessage = "Tariffa Standard";
+        if (age === "minorenne") offerMessage = "Tariffa Minorenni";
+        if (age === "over-65") offerMessage = "Tariffa Over 65";
+        
+        // Registrazione costo biglietto in base alla tariffa
+        let price = pricePerKm * kms;
+        console.log("Prezzo pieno: " + price);
+        if (age === "minorenne") price *= offerMinor;
+        if (age === "over-65") price *= offerOver65;
+        
+        // Registrazione numero cabina
+        const cabMin = 1;
+        const cabMax = 9;
+        
+        // Registrazione codice CP
+        const cpMin = 10000;
+        const cpMax = 20000;
+        
+        // 3. Stampare su schermo il riepilogo dati
+        passenger.innerText = name;
+        offer.innerText = offerMessage;
+        finalPrice.innerText = price.toFixed(2) + "€";
+        cab.innerText = Math.floor(Math.random() * (cabMax + 1 - cabMin)) + cabMin;
+        cpCode.innerText = Math.floor(Math.random() * (cpMax + 1 - cpMin)) + cpMin;
+    } else {
+        alert(errorMessage);
+    }
 })
 
-// 3. Settare il pulsante "reset"
+// 4. Settare il pulsante "reset"
 
 resetButton.addEventListener('click', function(){
     nameInput.value = "";
