284df3ffa9fe1c6bb9c521a4df46b8d1c7a51e27
SaBobic <fabrizioettori@gmail.com>
2022-06-22 15:22:50 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+
+Il programma dovrà mostrare un form da compilare con cui chiedere:
+- il numero di chilometri che vuole percorrere
+- l'età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- 0.21 € al km
+- sconto del 20% per i minorenni
+- sconto del 40% per gli over 65
+Il recap dei dati e l'output del prezzo finale va stampato in pagina formattato con massimo due decimali.
+
+1. 
+*/
